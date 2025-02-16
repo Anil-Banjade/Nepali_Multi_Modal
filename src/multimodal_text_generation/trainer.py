5fbea9fdfcbd6f3594010a7f7852f080438e1632
Anil Banjade <anil.banjade17@gmail.com>
2025-02-16 21:48:05 +0545

    changes to outputs

--- src/multimodal_text_generation/trainer.py
+++ src/multimodal_text_generation/trainer.py
@@ -25,7 +25,7 @@ def train_model(model,dataloader,valid_loader,num_epochs,device):
       target_ids=tokens['input_ids'].to(device) 
 
       outputs=model(fused_emb,target_ids[:,:-1]) 
-      outputs = outputs[:, 1:, :]
+      # outputs = outputs[:, 1:, :]
  
       loss = criterion(outputs.reshape(-1, config.vocab_size), target_ids[:, 1:].contiguous().view(-1))
       optimizer.zero_grad()
--- src/multimodal_text_generation/utils/inference.py
+++ src/multimodal_text_generation/utils/inference.py
@@ -83,3 +83,4 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"Using device: {device}")
 
 
+ 
